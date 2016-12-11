795c9d7271cb4b6d2793c1d7d9b9cda8dbdaadfc
vanbleusa <a.vanbleus@gmail.com>
2016-12-11 21:43:57 +0100

    ajout accept token header

--- back/service_gateway/app.js
+++ back/service_gateway/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, token, Accept");
   next();
 });
 
